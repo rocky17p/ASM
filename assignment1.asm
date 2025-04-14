ddd83932b4685fd1f66968ff2973bfb76d99b3b2
rishipatwa17p <rishi17p@gmail.com>
2025-04-14 19:52:12 +0530

    Update assignment1.asm

--- assignment1.asm
+++ assignment1.asm
@@ -1,31 +1,28 @@
 section .data
-	msg db "enter 5 64 bit numbers",0xA
-	len equ $-msg
+        m1 db "enter 5 64 bit number",10
+        len equ $-m1
 section .bss
-	var resb 85
+        var resb 85
 section .text
-	global _start
-	%macro func 4
-		mov rax,%1
-		mov rdi,%2
-		mov rsi,%3
-		mov rdx,%4
-		syscall
-	%endmacro
+        global _start
+        %macro rw 3
+        mov rax,%1
+        mov rdi,%1
+        mov rsi,%2
+        mov rdx,%3
+        syscall
+        %endmacro
 _start:
-	func 1,1,msg,len
-	mov r8,5
-	mov rsi,var
-loop_start:
-	func 0,0,rsi,17
-	func 1,1,rsi,17
-	add rsi,17
-	dec r8
-	jnz loop_start
-	
-	
-	mov rax,60
-	mov rdi,0
-	syscall
-	
-	
+        rw 1,m1,len
+        mov r8,5
+        mov rsi,var
+start_loop:
+        rw 0,rsi,17
+        rw 1,rsi,17
+        add rsi,17
+        dec r8
+        jnz start_loop
+
+        mov rax,60
+        mov rdi,0
+        syscall
